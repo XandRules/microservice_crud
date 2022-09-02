47687e6fca36daad8c3ee525f050b135626591f1
Alexandre Ribeiro <alexandresilva58@gmail.com>
2022-09-01 21:24:56 -0300

    Add configuração RabbitMQ para envio de messagem de produto

--- src/main/java/com/xandrules/crud/services/ProdutoService.java
+++ src/main/java/com/xandrules/crud/services/ProdutoService.java
@@ -3,6 +3,7 @@ package com.xandrules.crud.services;
 import com.xandrules.crud.data.vo.ProdutoVO;
 import com.xandrules.crud.entity.Produto;
 import com.xandrules.crud.exception.ResourceNotFoundException;
+import com.xandrules.crud.message.ProdutoSendMessage;
 import com.xandrules.crud.repository.ProdutoRepository;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +11,24 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 
 @Service
 public class ProdutoService {
 
     private final ProdutoRepository produtoRepository;
+    private final ProdutoSendMessage produtoSendMessage;
 
     @Autowired
-    public ProdutoService(ProdutoRepository produtoRepository) {
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoSendMessage produtoSendMessage) {
         this.produtoRepository = produtoRepository;
+        this.produtoSendMessage = produtoSendMessage;
     }
 
-    public ProdutoVO create(ProdutoVO produto){
+    public ProdutoVO create(ProdutoVO produtoVO){
 
-        return ProdutoVO.create(produtoRepository.save(Produto.create(produto)));
+        ProdutoVO produtoVORetorno = ProdutoVO.create(produtoRepository.save(Produto.create(produtoVO)));
+        produtoSendMessage.sendMessage(produtoVO);
+        return produtoVORetorno;
     }
 
     public Page<ProdutoVO> findAll(Pageable pageable){
